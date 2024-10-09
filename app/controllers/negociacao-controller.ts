a6b0a3071acaa284484c1ec5ee1d9c95ae752e1e
HeberthDev <heberthdeveloper@gmail.com>
2024-10-09 07:56:07 -0300

    Mudando projeto para o que já foi feito na máquina

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,5 +1,5 @@
-import { Negociacao } from '../models/negociacao.js';
-import { Negociacoes } from '../models/negociacoes.js';
+import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
@@ -8,31 +8,38 @@ export class NegociacaoController {
     private negociacoes = new Negociacoes();
 
     constructor() {
-        this.inputData = document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valor');
+        this.inputData = document.querySelector( '#data' );
+        this.inputQuantidade = document.querySelector( '#quantidade' );
+        this.inputValor = document.querySelector( '#valor' );
     }
 
     adiciona(): void {
         const negociacao = this.criaNegociacao();
+
         negociacao.data.setDate(12);
-        this.negociacoes.adiciona(negociacao);
-        console.log(this.negociacoes.lista());
+        this.negociacoes.adiciona( negociacao );
+        console.log( this.negociacoes.lista() );
         this.limparFormulario();
     }
 
     criaNegociacao(): Negociacao {
         const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);
+        const date = new Date( this.inputData.value.replace( exp, ',' ) );
+        const quantidade = parseInt( this.inputQuantidade.value );
+        const valor = parseFloat( this.inputValor.value );
+
+        return new Negociacao(
+            date,
+            quantidade,
+            valor,
+        );
     }
 
     limparFormulario(): void {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
+
         this.inputData.focus();
     }
 }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -7,6 +7,7 @@ export class Negociacao {
     get volume() {
         return this.quantidade * this.valor;
     }
+    //Utilizando pra uma programação defensiva. Onde, se outro desenvolvedor tentar modificar esse valor de data, como por exemplo utilizando "negociacao.data.setDate(12)" onde o desenvolvedor força o dia ser sempre 12, ele não vai conseguir mudar o valor original da data.
     get data() {
         const data = new Date(this._data.getTime());
         return data;
