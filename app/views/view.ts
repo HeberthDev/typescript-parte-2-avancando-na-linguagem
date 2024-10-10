478a328f5adc0290366adff41ceade5a4de8ec08
HeberthDev <heberthdeveloper@gmail.com>
2024-10-10 08:07:56 -0300

    Mais surpresas com Herança

--- app/views/view.ts
+++ app/views/view.ts
@@ -4,4 +4,13 @@ export class View {
     constructor( seletor: string ) {
         this.elemento = document.querySelector( seletor );
     }
+
+    template( model: string ): string {
+        throw Error( 'Classe filha precisa implementar o método template' );
+    }
+
+    update( model: string ): void {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
 }
