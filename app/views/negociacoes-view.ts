f330774f2e729e3f55e172606a874aadd66b07df
HeberthDev <heberthdeveloper@gmail.com>
2024-10-11 07:41:02 -0300

    Visibilidade do método template

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -2,8 +2,8 @@ import { Negociacoes } from "../models/negociacoes.js";
 import { View } from "./view.js";
 
 export class NegociacoesView extends View<Negociacoes> {
-
-    template( model: Negociacoes ): string {
+    // Está como protected também, igual a classe "mãe" View, porquê a classe filha se não colocar nada, de forma implícita é public. Então a classe filha está mudando um método protected pra public. Por isso que temos que colocar "protected" nas classes filhas também para não ser visto por outro lugares que não poderia ver sem necessidade.
+    protected template( model: Negociacoes ): string {
         return `
             <table class="table table-hover table-bordered">
                 <thead>
