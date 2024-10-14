870b93ce0f4a22159dd0b02ce92bdc943a52a60f
HeberthDev <heberthdeveloper@gmail.com>
2024-10-14 07:38:13 -0300

    Isolando a lógica de conversão de entrada

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -14,7 +14,8 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
     adiciona() {
-        const negociacao = this.criaNegociacao();
+        const negociacaoTemporaria = new Negociacao(null, 0, 0);
+        const negociacao = negociacaoTemporaria.criaDe(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
         //negociacao.data.setDate(12); Exemplo de tentativa de modificação forçada do valor de data
         if (!this.ehDiaUtil(negociacao.data)) {
             this.mensagemView.update('Apenas negociações em dias úteis são aceitas');
@@ -26,13 +27,6 @@ export class NegociacaoController {
     }
     ehDiaUtil(data) {
         return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO;
-    }
-    criaNegociacao() {
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);
     }
     limparFormulario() {
         this.inputData.value = '';
